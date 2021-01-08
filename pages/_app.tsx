88cfb8cbd5a7c5fb5cc4713be6e68f9fcce10f62
Christopher Laeri <claeri27@gmail.com>
2021-01-08 01:04:10 -0500

    🎉 init

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import '@/styles/globals.css'
+import React, { ReactElement } from 'react'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const MyApp = ({ Component, pageProps }: AppProps): ReactElement => (
+  <QueryClientProvider client={new QueryClient()}>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </QueryClientProvider>
+)
+
+export default MyApp
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest as Req, NextApiResponse as Res } from 'next'
 
-export default (req, res) => {
+export default (req: Req, res: Res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
