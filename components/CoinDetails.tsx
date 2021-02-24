22ff821ad232f3ad65ab0723beb0c4518166c678
Christopher Laeri <claeri27@gmail.com>
2021-02-24 12:36:46 -0500

    💄 improved coindetails on mobile

--- components/CoinDetails.tsx
+++ components/CoinDetails.tsx
@@ -1,4 +1,13 @@
-import { AccordionPanel, Flex, Img, SimpleGrid, Spinner, Text } from '@chakra-ui/react'
+import {
+  AccordionPanel,
+  Flex,
+  FlexProps,
+  Img,
+  SimpleGrid,
+  Spinner,
+  Text,
+  TextProps,
+} from '@chakra-ui/react'
 import React, { FC } from 'react'
 import type { Coin } from '@/types'
 import { formatDate, formatNum } from '@/lib/formatData'
@@ -14,60 +23,72 @@ const Sparklines = dynamic(() => import('@/components/Sparklines'), {
   ),
 })
 
+const GridItem: FC<FlexProps> = ({ children, ...props }) => (
+  <Flex {...props} direction="column" justify="center" align="center" height="80px">
+    {children}
+  </Flex>
+)
+
+const GridItemHeader: FC<TextProps> = ({ children, ...props }) => (
+  <Text {...props} fontSize={['xs', null, 'sm']}>
+    {children}
+  </Text>
+)
+const GridItemValue: FC<TextProps> = ({ children, ...props }) => (
+  <Text {...props} fontSize={['lg', null, 'xl']}>
+    {children}
+  </Text>
+)
+
 const CoinDetails: FC<Coin> = props => {
   return (
-    <AccordionPanel>
-      <Flex justify="space-between" align="center" my="1rem">
-        <Flex align="center">
-          <Img ml="1rem" src={props.image} alt="missing" w={200} h={200} />
-          <Flex px={5} direction="column">
-            <Text fontSize="3xl">{`${props.name} (${props.symbol.toUpperCase()})`}</Text>
+    <AccordionPanel p="0rem">
+      <Flex
+        p=".5rem"
+        justify="space-around"
+        direction={['column', null, null, 'row']}
+        align="center"
+        my="1rem">
+        <Flex w={['100%', null, null, 'auto']} mb="2rem" justify="center" align="center">
+          <Img mr="1rem" src={props.image} alt="missing" w={[120, 120, 200]} h={[120, 120, 200]} />
+          <Flex ml="1rem" direction="column">
+            <Text fontSize="2xl">{`${props.name} (${props.symbol.toUpperCase()})`}</Text>
             <Text fontSize="2xl">{formatNum(props.current_price, 2, 2, false)}</Text>
           </Flex>
         </Flex>
-        <SimpleGrid columns={4} spacing={10}>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">CURRENT PRICE</Text>
-            <Text fontSize="2xl">{formatNum(props.current_price, 4, 2)}</Text>
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">MARKET CAP</Text>
-            <Text fontSize="2xl">{formatNum(props.market_cap)}</Text>
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">TOTAL VOLUME</Text>
-            <Text fontSize="2xl">{formatNum(props.total_volume)}</Text>
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">TOTAL SUPPLY</Text>
-            <Text fontSize="2xl">{formatNum(props.total_supply)}</Text>
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">24HR HIGH</Text>
-            <Text fontSize="2xl">{formatNum(props.high_24h)}</Text>
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">24HR LOW</Text>
-            <Text fontSize="2xl">{formatNum(props.low_24h)}</Text>
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">ALL TIME HIGH</Text>
-            <Text fontSize="2xl" px="1rem">
-              {formatNum(props.ath)}
-            </Text>
-            {/* <Flex>
-              <Text>{formatNum(props.ath_change_percentage, 2, 2, true)}</Text>
-              <Text>{formatDate(props.ath_date)}</Text>
-            </Flex> */}
-          </Flex>
-          <Flex direction="column" justify="center" align="center" height="80px">
-            <Text fontSize="sm">ALL TIME LOW</Text>
-            <Text fontSize="2xl">{formatNum(props.atl)}</Text>
-            {/* <Flex>
-              <Text mr="1rem">{formatNum(props.atl_change_percentage, 2, 2, true)}</Text>
-              <Text>{formatDate(props.atl_date)}</Text>
-            </Flex> */}
-          </Flex>
+        <SimpleGrid mb={['2rem', '0rem']} columns={[2, 4]} spacing={[10, 3, 10]}>
+          <GridItem>
+            <GridItemHeader>PRICE</GridItemHeader>
+            <GridItemValue>{formatNum(props.current_price, 4, 2)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>MARKET CAP</GridItemHeader>
+            <GridItemValue>{formatNum(props.market_cap)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>TOTAL VOLUME</GridItemHeader>
+            <GridItemValue>{formatNum(props.total_volume)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>TOTAL SUPPLY</GridItemHeader>
+            <GridItemValue>{formatNum(props.total_supply)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>24HR HIGH</GridItemHeader>
+            <GridItemValue>{formatNum(props.high_24h)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>24HR LOW</GridItemHeader>
+            <GridItemValue>{formatNum(props.low_24h)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>ALL TIME HIGH</GridItemHeader>
+            <GridItemValue>{formatNum(props.ath)}</GridItemValue>
+          </GridItem>
+          <GridItem>
+            <GridItemHeader>ALL TIME LOW</GridItemHeader>
+            <GridItemValue>{formatNum(props.atl)}</GridItemValue>
+          </GridItem>
         </SimpleGrid>
       </Flex>
       <Sparklines coin={props} />
